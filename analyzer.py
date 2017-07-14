fdea5cb5d5eabf9fe33c9f3a331ff4d73e12439d
tonygeegle <253106788@qq.com>
2017-07-14 19:21:36 +0800

    sss

--- analyzer.py
+++ analyzer.py
@@ -3,16 +3,22 @@
 import os
 import fileinput
 import csv
-import simplejson as json
 
+class UserInfo(object):
+	def __init__(self, code, count):
+		self.code = code
+		self.count = count
+
+result = {}
 logFiles = [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.txt']
 #print(logFiles)
 with fileinput.input(files=(logFiles)) as f:
 	sum = 0
 	for line in f:
-		lineDic = json.loads(line[1:-2])
+		lineDic = eval(line[1:-2])
 		user_key = lineDic["sNetNo"]
-		ser_value = result.setdefault(user_key, 0)
+		ser_value = result.setdefault(user_key, UserInfo(user_key, 0))
+		break
 		result[user_key] += 1
 		sum += 1
 		if fileinput.isfirstline():
@@ -21,7 +27,6 @@ with fileinput.input(files=(logFiles)) as f:
 		per_count += 1
 		if per_count % 10000 == 0:
 			print("已经扫描了", per_count, "行.")
-	print("已经扫描了", per_count - 1, "行. ")
 print("共扫描了", len(logFiles), "个文件.", sum, "行数据. 扫描完毕!")
 
 with open('result.csv', 'w', newline='') as csvfile:
