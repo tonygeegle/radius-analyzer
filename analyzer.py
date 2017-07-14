8429d0616f8c3b70d0f57337c3c061d178dd7c26
tonygeegle <253106788@qq.com>
2017-07-14 17:02:42 +0800

    update .gitignore

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import fileinput
+import csv
+import simplejson as json
 
-class Student(object):
+result = {}
+lineDic = {}
+logFiles = [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.log']
+#print(logFiles)
+with fileinput.input(files=(logFiles)) as f:
+	sum = 0
+	for line in f:
+		lineDic = json.loads(line[1:-2])
+		user_key = lineDic["sNetNo"]
+		ser_value = result.setdefault(user_key, 0)
+		result[user_key] += 1
+		sum += 1
+		if fileinput.isfirstline():
+			per_count = 1
+			print("开始扫描文件", fileinput.filename())
+		per_count += 1
+		if per_count % 10000 == 0:
+			print("已经扫描了", per_count, "行.")
+	print("已经扫描了", per_count - 1, "行. ")
+print("共扫描了", len(logFiles), "个文件.", sum, "行数据. 扫描完毕!")
 
-    def get_score(self):
-         return self._score
-
-    def set_score(self, value):
-        if not isinstance(value, int):
-            raise ValueError('score must be an integer!')
-        if value < 0 or value > 100:
-            raise ValueError('score must between 0 ~ 100!')
-        self._score = value
-
-s = Student()
-s.set_score(100)
-print(s.get_score())
-
+with open('result.csv', 'w', newline='') as csvfile:
+	spamwriter = csv.writer(csvfile, delimiter=',')
+	for item in result.items():
+		spamwriter.writerow(item)
+print("结果已经保存到 result.txt , 共写入了", len(result), "行数据!")
