864735626cde4cf82f8afa65a9211ad43b798f42
tonygeegle <253106788@qq.com>
2017-08-29 09:02:50 +0800

    add more function

--- analyzer.py
+++ analyzer.py
@@ -6,11 +6,17 @@ import csv
 import datetime
 
 class UserInfo(object):
-	def __init__(self, code, count):
+	def __init__(self, code, count, output, input):
+		#code为宽带账号
 		self.code = code
-		self.count = count
+		#session_count为会话次数
+		self.session_count = count
+		#output_octets 为下行字节数
+		self.output_octets = output
+		#input_octets 为上行字节数
+		self.input_octets = input
 	def getInfo(self):
-		return self.code, self.count
+		return self.code, self.session_count, self.output_octets, self.input_octets
 
 result = {}
 logFiles = [x for x in os.listdir('.') if os.path.isfile(x) and os.path.getsize(x) > 0 and os.path.splitext(x)[1]=='.txt']
@@ -19,9 +25,11 @@ with fileinput.input(files=(logFiles)) as f:
 	sum = 0
 	for line in f:
 		lineDic = eval(line[1:-2])
-		user_key = lineDic["sNetNo"]
-		ser_value = result.setdefault(user_key, UserInfo(user_key, 0))
-		ser_value.count += 1
+		user_key = lineDic["net_no"]
+		ser_value = result.setdefault(user_key, UserInfo(user_key, 0, 0, 0))
+		ser_value.session_count += 1
+		ser_value.output_octets += lineDic["acct_output_octets"]
+		ser_value.input_octets += lineDic["acct_input_octets"]
 		sum += 1
 		if fileinput.isfirstline():
 			per_count = 1
